8d1de3f2e8f10652aaaca41e99f307d8cd1a11db
gayatrisamal7 <151025542+gayatrisamal7@users.noreply.github.com>
2025-08-10 10:54:31 +0200

    add new code

--- quiz-frontend/src/firebase/firebase.jsx
+++ quiz-frontend/src/firebase/firebase.jsx
@@ -8,13 +8,13 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REDACTED",
-  authDomain: "quiz-app-gayatri.firebaseapp.com",
-  projectId: "quiz-app-gayatri",
-  storageBucket: "quiz-app-gayatri.firebasestorage.app",
-  messagingSenderId: "728078397266",
-  appId: "1:728078397266:web:b5b7fc3572e1e3690a548b",
-  measurementId: "G-Z4NQE4BQN7"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
